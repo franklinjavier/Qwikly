bec54278cecfc90618070e1998e717875443b9aa
Frank <franklinjalves@gmail.com>
2023-02-18 10:08:58 +0000

    refactor: base url on link

--- src/routes/shorts/[short]/index.tsx
+++ src/routes/shorts/[short]/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik'
-import type { DocumentHead } from '@builder.io/qwik-city'
+import { DocumentHead, useLocation } from '@builder.io/qwik-city'
 import { loader$ } from '@builder.io/qwik-city'
 import { supabase } from '~/supabase'
 
@@ -17,12 +17,21 @@ export const useShort = loader$(async (event) => {
 })
 
 export default component$(() => {
+  const location = useLocation()
+
   const short = useShort().value
+  const shortLink = `${location.url.origin}/${short.link}`
 
   return (
     <div class="relative isolate px-6 py-24 shadow-2xl sm:px-24 xl:py-32 h-screen gap-2 flex flex-col">
       <span>
-        Shorten URL: <a href={`/${short.link}`}>{short.link}</a>
+        Short URL:{' '}
+        <a
+          href={shortLink}
+          class="text-indigo-600 hover:underline"
+        >
+          {shortLink}
+        </a>
       </span>
       <span>Destination: {short.destination}</span>
     </div>
